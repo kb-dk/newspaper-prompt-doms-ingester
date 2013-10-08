2b1d6d494f1fd175aa53d340d45d59afdbaf55e3
Colin Rosenthal <csr@statsbiblioteket.dk>
2013-10-08 09:43:15 +0200

    Added (not-yet-functional) cleanup methods to integration test.

--- src/test/java/dk/statsbiblioteket/newspaper/promptdomsingester/SimpleFedoraIngesterTestIntegration.java
+++ src/test/java/dk/statsbiblioteket/newspaper/promptdomsingester/SimpleFedoraIngesterTestIntegration.java
@@ -1,22 +1,35 @@
 package dk.statsbiblioteket.newspaper.promptdomsingester;
 
-import dk.statsbiblioteket.doms.central.connectors.EnhancedFedora;
-import dk.statsbiblioteket.doms.central.connectors.EnhancedFedoraImpl;
+import dk.statsbiblioteket.doms.central.connectors.*;
 import dk.statsbiblioteket.doms.central.connectors.fedora.pidGenerator.PIDGeneratorException;
 import dk.statsbiblioteket.doms.webservices.authentication.Credentials;
+import dk.statsbiblioteket.newspaper.RecursiveFedoraCleaner;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import javax.xml.bind.JAXBException;
 import java.net.MalformedURLException;
 
 /**
- * Created with IntelliJ IDEA.
- * User: csr
- * Date: 10/4/13
- * Time: 2:10 PM
- * To change this template use File | Settings | File Templates.
+ *
  */
 public class SimpleFedoraIngesterTestIntegration extends SimpleFedoraIngesterTest {
+
+    @BeforeMethod
+    public void setup() throws MalformedURLException, JAXBException, BackendInvalidCredsException, BackendMethodFailedException, BackendInvalidResourceException, PIDGeneratorException {
+        cleanupFedora();
+    }
+
+    @AfterMethod
+    public void teardown() throws MalformedURLException, JAXBException, BackendInvalidCredsException, BackendMethodFailedException, BackendInvalidResourceException, PIDGeneratorException {
+         cleanupFedora();
+    }
+
+    public void cleanupFedora() throws MalformedURLException, JAXBException, PIDGeneratorException, BackendInvalidCredsException, BackendMethodFailedException, BackendInvalidResourceException {
+        String label = "dir:B400022028241-RT1";
+        RecursiveFedoraCleaner.cleanFedora(getEnhancedFedora(), label, true);
+    }
 
     @Override
     public EnhancedFedora getEnhancedFedora() throws MalformedURLException, JAXBException, PIDGeneratorException {
@@ -30,5 +43,6 @@ public class SimpleFedoraIngesterTestIntegration extends SimpleFedoraIngesterTes
     public void testIngest() throws Exception {
         super.testIngest();
         String pid = super.pid;
+        //Now do some clever testing here. e.g. tree-parse doms and see if we get the same structure back.
     }
 }
