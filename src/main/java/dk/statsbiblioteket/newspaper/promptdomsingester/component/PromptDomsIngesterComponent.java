63c669ed3615a8bf9fbf3dccab08b1900f795e46
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-10-09 19:27:02 +0200

    Include the B in batch id

--- src/main/java/dk/statsbiblioteket/newspaper/promptdomsingester/component/PromptDomsIngesterComponent.java
+++ src/main/java/dk/statsbiblioteket/newspaper/promptdomsingester/component/PromptDomsIngesterComponent.java
@@ -154,7 +154,7 @@ public class PromptDomsIngesterComponent implements RunnableComponent {
     public void doWorkOnBatch(Batch batch, ResultCollector resultCollector) throws Exception {
         Long batchId = batch.getBatchID();
         Integer rt = batch.getRoundTripNumber();
-        File rootFile = new File(batchSuperDirectory, batchId + "-RT" + rt);
+        File rootFile = new File(batchSuperDirectory, "B"+batchId + "-RT" + rt);
         if (!rootFile.exists()) {
             resultCollector.addFailure(batchId+"", "No Such File", getComponentName(), "File " + rootFile.getAbsolutePath() + " doesn't exist.");
             resultCollector.setSuccess(false);
