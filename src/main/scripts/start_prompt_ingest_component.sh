509f8e98522fc7800df3422e845506060f22b8b3
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2013-11-11 15:49:07 +0100

    Fix spelling error

--- src/main/scripts/start_prompt_ingest_component.sh
+++ src/main/scripts/start_prompt_ingest_component.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
-java java -classpath "$SCRIPT_DIR/../conf:$SCRIPT_DIR/../lib/*" \
+java -classpath "$SCRIPT_DIR/../conf:$SCRIPT_DIR/../lib/*" \
  dk.statsbiblioteket.newspaper.promptdomsingester.component.PromptDomsIngesterComponent \
  $SCRIPT_DIR/../conf/config.properties
