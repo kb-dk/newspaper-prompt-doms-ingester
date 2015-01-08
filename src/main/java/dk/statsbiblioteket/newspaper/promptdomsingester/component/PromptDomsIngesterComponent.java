8b058977e9431e2c4a0fb2e911cf8129cf792cef
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-01-08 10:55:29 +0100

    Use new fedora that retries on 409 errors

--- src/main/java/dk/statsbiblioteket/newspaper/promptdomsingester/component/PromptDomsIngesterComponent.java
+++ src/main/java/dk/statsbiblioteket/newspaper/promptdomsingester/component/PromptDomsIngesterComponent.java
@@ -48,11 +48,13 @@ public class PromptDomsIngesterComponent {
                 properties.getProperty(ConfigConstants.DOMS_USERNAME),
                 properties.getProperty(ConfigConstants.DOMS_PASSWORD));
         String fedoraLocation = properties.getProperty(ConfigConstants.DOMS_URL);
+        int fedoraRetries = Integer.parseInt(properties.getProperty(ConfigConstants.FEDORA_RETRIES, "1"));
+        int fedoraDelayBetweenRetries = Integer.parseInt(properties.getProperty(ConfigConstants.FEDORA_DELAY_BETWEEN_RETRIES, "100"));
         EnhancedFedoraImpl eFedora = new EnhancedFedoraImpl(
                 creds,
                 fedoraLocation,
                 properties.getProperty(ConfigConstants.DOMS_PIDGENERATOR_URL),
-                null);
+                null, fedoraRetries, fedoraRetries, fedoraRetries, fedoraDelayBetweenRetries);
 
         RunnableComponent component = new RunnableMultiThreadedPromptDomsIngester(properties, eFedora);
         CallResult result = NewspaperBatchAutonomousComponentUtils.startAutonomousComponent(properties, component);
